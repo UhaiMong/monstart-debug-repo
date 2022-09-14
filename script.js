5743526d890bd88e986fffe6489d278f5c5ec003
UhaiMong <uhaimarma2016@gmail.com>
2022-09-15 04:47:57 +0600

    blog page question ans: updated

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ fetch("./texts.json")
 // checks the user typed character and displays accordingly
 const typeController = (e) => {
   const newLetter = e.key;
-
+  
+  const totalWord = userText.split(',');
   // Handle backspace press
   if (newLetter == "Backspace") {
     userText = userText.slice(0, userText.length - 1);
